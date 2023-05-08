952a04e04a991610f0d2692c791a5c35efab5d2a
Brijen Makwana <brijenmakwana@192.168.1.7>
2023-05-08 16:37:38 +0530

    select car options now only visible when user press Done

--- src/components/UIComponents/UIButton.tsx
+++ src/components/UIComponents/UIButton.tsx
@@ -1,11 +1,11 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text } from "react-native";
 import React, { FC } from "react";
 import { UIButtonProps } from "../../types/types";
 
 const UIButton: FC<UIButtonProps> = (props) => {
-  const { text } = props;
+  const { text, onPress } = props;
   return (
-    <Pressable style={styles.container}>
+    <Pressable style={styles.container} onPress={onPress}>
       <Text style={styles.text}>{text}</Text>
     </Pressable>
   );
